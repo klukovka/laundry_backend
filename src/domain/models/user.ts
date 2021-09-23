1a82c3d090d9d72e95ce9756146036cfe7403c68
Yuliia Koba <yuliia.koba@nure.ua>
2021-09-23 13:51:26 +0300

    add UserRepository

--- src/domain/models/user.ts
+++ src/domain/models/user.ts
@@ -1,11 +1,18 @@
 export class User {
   idUser?: string | null;
   email: string;
-  password: string;
+  password?: string | null;
+  role: string;
 
-  constructor(email: string, password: string, idUser: string | null = null) {
+  constructor(
+    email: string,
+    role: string,
+    idUser: string | null = null,
+    password: string | null = null
+  ) {
     this.idUser = idUser;
     this.email = email;
     this.password = password;
+    this.role = role;
   }
 }
