889d1aeb7c29538beec6506ec961e52a307e3a2f
Yuliia Koba <yuliia.koba@nure.ua>
2021-09-23 18:30:56 +0300

    add CRUD for user mongo

---
+++ src/data/dataSource/mongoDB/models/user.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose';
+
+const User = new Schema({
+  _id: Schema.Types.ObjectId,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match:
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+  },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+});
+
+export default model('User', User);
