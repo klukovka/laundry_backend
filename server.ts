aee59ef0e42c5ca34c580833ee45b1d3ce210e8d
Yuliia Koba <yuliia.koba@nure.ua>
2021-10-14 22:23:14 +0300

    add host to the server

--- server.ts
+++ server.ts
@@ -6,11 +6,12 @@ import App from './src/features/app';
 env.config();
 
 const PORT = process.env.PORT;
+const HOST: string = process.env.HOST as string;
 
 const server = (app: Application) => {
   DatabaseMongo.getDB;
   app
-    .listen(PORT, () => {
+    .listen(Number(PORT), HOST, () => {
       console.log('Server is listening...');
     })
     .once('close', () => DatabaseMongo.getDB.close());
