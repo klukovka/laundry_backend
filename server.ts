2999af44ed1ff4f3c41a3f5a8666f6a1a46a7aff
Yuliia Koba <yuliia.koba@nure.ua>
2021-10-14 23:28:01 +0300

    add http server

--- server.ts
+++ server.ts
@@ -2,19 +2,17 @@ import env from 'dotenv';
 import { Application } from 'express';
 import { DatabaseMongo } from './src/data/dataSource/mongoDB/databaseMongo';
 import App from './src/features/app';
+import https from 'https';
+import http from 'http';
 
 env.config();
 
 const PORT = process.env.PORT;
 const HOST: string = process.env.HOST as string;
 
-const server = (app: Application) => {
-  DatabaseMongo.getDB;
-  app
-    .listen(Number(PORT), HOST, () => {
-      console.log('Server is listening...');
-    })
-    .once('close', () => DatabaseMongo.getDB.close());
-};
-
-server(App);
+http
+  .createServer(App)
+  .listen(Number(PORT), HOST, () => {
+    console.log('Server is listening...');
+  })
+  .once('close', () => DatabaseMongo.getDB.close());
