9dbf049ed4f914783442553151474897a62d4a5c
Yuliia Koba <yuliia.koba@nure.ua>
2021-10-14 23:44:32 +0300

    add ssl

--- server.ts
+++ server.ts
@@ -4,14 +4,20 @@ import { DatabaseMongo } from './src/data/dataSource/mongoDB/databaseMongo';
 import App from './src/features/app';
 import https from 'https';
 import http from 'http';
+import fs from 'fs';
 
 env.config();
 
 const PORT = process.env.PORT;
 const HOST: string = process.env.HOST as string;
 
-http
-  .createServer(App)
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+};
+
+https
+  .createServer(options, App)
   .listen(Number(PORT), HOST, () => {
     console.log('Server is listening...');
   })
