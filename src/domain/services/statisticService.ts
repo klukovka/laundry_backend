c64a8f28b4112223cfc4f659305fb143266114f4
Yuliia Koba <yuliia.koba@proarea.com>
2022-05-15 17:39:19 +0300

    laundryTimeAndUsageStatistic

--- src/domain/services/statisticService.ts
+++ src/domain/services/statisticService.ts
@@ -1,6 +1,7 @@
 import { StatisticLaundry } from '../models/statisticLaundry';
 import { WashMachine } from '../models/washMachine';
 import { Laundry } from '../models/laundry';
+import { Event } from '../models/event';
 import { PagedModel } from '../models/pagedModel';
 import { EventRepository } from '../repositories/eventRepository';
 import { LaundryRepository } from '../repositories/laundryRepository';
@@ -26,7 +27,7 @@ export class StatisticService {
       const totalElements = await this._laundryRepository.getLaundriesAmount();
       const laundries = await this._laundryRepository.getLaundries(page, size);
 
-      const content = new Array<StatisticLaundry>();
+      const content = new Array<StatisticLaundry<number>>();
       if (laundries) {
         for (let i = 0; i < laundries.length; i++) {
           content.push(
@@ -95,12 +96,7 @@ export class StatisticService {
         eventsAmount
       );
 
-      let sum = 0;
-      for (let i = 0; i < events.length; i++) {
-        sum += events[i]?.rating ?? 0;
-      }
-
-      return sum / eventsAmount;
+      return this._countRating(events, eventsAmount);
     } catch (error) {
       throw error;
     }
@@ -122,12 +118,241 @@ export class StatisticService {
         eventsAmount
       );
 
-      let sum = 0;
-      for (let i = 0; i < events.length; i++) {
-        sum += events[i]?.rating ?? 0;
+      return this._countRating(events, eventsAmount);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private _countRating(events: Event[], eventsAmount: number) {
+    let sum = 0;
+    for (let i = 0; i < events.length; i++) {
+      sum += events[i]?.rating ?? 0;
+    }
+
+    return sum / eventsAmount;
+  }
+
+  async allLaundryPaymentStatistic(
+    query: any
+  ): Promise<PagedModel<StatisticLaundry<any>>> {
+    try {
+      const page = Number(query.page ?? 0);
+      const size = Number(query.size ?? 15);
+      const totalElements = await this._laundryRepository.getLaundriesAmount();
+      const laundries = await this._laundryRepository.getLaundries(page, size);
+
+      const content = new Array<StatisticLaundry<any>>();
+      if (laundries) {
+        for (let i = 0; i < laundries.length; i++) {
+          content.push(
+            await this.laundryPaymentStatistic(laundries[i].laundryId!)
+          );
+        }
       }
 
-      return sum / eventsAmount;
+      return new PagedModel<StatisticLaundry<any>>(
+        page,
+        size,
+        Math.ceil(totalElements / size),
+        totalElements,
+        content
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async laundryPaymentStatistic(
+    laundryId: string
+  ): Promise<StatisticLaundry<any>> {
+    try {
+      const laundry = await this._laundryRepository.getLaundryById(laundryId);
+
+      const washMachinesAmount =
+        await this._laundryRepository.getWashMachinesAmount(laundryId);
+      const washMachines = await this._laundryRepository.getWashMachines(
+        laundryId,
+        0,
+        washMachinesAmount
+      );
+
+      const laundryPayment = await this._laundryPayments(laundry!);
+
+      let washMachinesPaymments = new Map<WashMachine, any>();
+
+      for (let i = 0; i < washMachines.length; i++) {
+        const payment = await this._washMachinePayments(washMachines[i]);
+        if (payment) {
+          washMachinesPaymments.set(washMachines[i], payment);
+        }
+      }
+
+      return new StatisticLaundry<any>(
+        laundry!,
+        laundryPayment,
+        washMachinesPaymments
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async _laundryPayments(laundry: Laundry): Promise<number> {
+    try {
+      const eventsAmount = await this._eventRepository.getLaundryEventsAmount(
+        laundry.laundryId!
+      );
+      if (eventsAmount == 0) return null;
+
+      const events = await this._eventRepository.getLaundryEvents(
+        laundry.laundryId!,
+        0,
+        eventsAmount
+      );
+
+      return this._countPayment(events);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async _washMachinePayments(washMachine: WashMachine): Promise<any> {
+    try {
+      const eventsAmount =
+        await this._eventRepository.getWashMachineEventsAmount(
+          washMachine.washMachineId!
+        );
+      if (eventsAmount == 0) return null;
+
+      const events = await this._eventRepository.getWashMachineEvents(
+        washMachine.washMachineId!,
+        0,
+        eventsAmount
+      );
+
+      return this._countPayment(events);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private _countPayment(events: Event[]): any {
+    let sum = { paidBonuses: 0, paidMoney: 0, all: 0 };
+    for (let i = 0; i < events.length; i++) {
+      sum.paidBonuses += events[i].paidBonuses;
+      sum.paidMoney += events[i].paidMoney;
+      sum.all += events[i].paidBonuses + events[i].paidMoney;
+    }
+
+    return sum;
+  }
+
+  async allLaundryTimeAndUsageStatistic(
+    query: any
+  ): Promise<PagedModel<StatisticLaundry<any>>> {
+    try {
+      const page = Number(query.page ?? 0);
+      const size = Number(query.size ?? 15);
+      const totalElements = await this._laundryRepository.getLaundriesAmount();
+      const laundries = await this._laundryRepository.getLaundries(page, size);
+
+      const content = new Array<StatisticLaundry<any>>();
+      if (laundries) {
+        for (let i = 0; i < laundries.length; i++) {
+          content.push(
+            await this.laundryTimeAndUsageStatistic(laundries[i].laundryId!)
+          );
+        }
+      }
+
+      return new PagedModel<StatisticLaundry<any>>(
+        page,
+        size,
+        Math.ceil(totalElements / size),
+        totalElements,
+        content
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async laundryTimeAndUsageStatistic(
+    laundryId: string
+  ): Promise<StatisticLaundry<any>> {
+    try {
+      const laundry = await this._laundryRepository.getLaundryById(laundryId);
+
+      const washMachinesAmount =
+        await this._laundryRepository.getWashMachinesAmount(laundryId);
+      const washMachines = await this._laundryRepository.getWashMachines(
+        laundryId,
+        0,
+        washMachinesAmount
+      );
+
+      let washMachinesTimeAndUsage = new Map<WashMachine, any>();
+
+      for (let i = 0; i < washMachines.length; i++) {
+        const timeAndUsage = await this._washMachineTimeAndUsage(
+          washMachines[i]
+        );
+        if (timeAndUsage) {
+          washMachinesTimeAndUsage.set(washMachines[i], timeAndUsage);
+        }
+      }
+
+      return new StatisticLaundry<any>(
+        laundry!,
+        this._laundryTimeAndUsage(washMachinesTimeAndUsage),
+        washMachinesTimeAndUsage
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private _laundryTimeAndUsage(map: Map<WashMachine, any>) {
+    let sum = { time: 0, powerUsage: 0 };
+
+    map.forEach((value: any, key: WashMachine) => {
+      sum.time += value.time;
+      sum.powerUsage += value.powerUsage;
+    });
+
+    return sum;
+  }
+
+  private async _washMachineTimeAndUsage(
+    washMachine: WashMachine
+  ): Promise<any> {
+    try {
+      const eventsAmount =
+        await this._eventRepository.getWashMachineEventsAmount(
+          washMachine.washMachineId!
+        );
+      if (eventsAmount == 0) return null;
+
+      const events = await this._eventRepository.getWashMachineEvents(
+        washMachine.washMachineId!,
+        0,
+        eventsAmount
+      );
+
+      let sum = { time: 0, powerUsage: 0 };
+
+      for (let i = 0; i < events.length; i++) {
+        const diff =
+          Math.abs(
+            events[i].timeEnd.getTime() - events[i].timeBegin.getTime()
+          ) /
+          1000 /
+          60;
+        sum.time += diff;
+        sum.powerUsage += diff * washMachine.powerUsage;
+      }
+      return sum;
     } catch (error) {
       throw error;
     }
